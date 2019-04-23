13d2ad14d52dc927a0d0ae2d457de4b2648d5405
BogdanFatu <ionutbogdan11@gmail.com>
2019-04-23 21:24:07 +0300

    Landing page --> Bootstrap template

--- OnlineShopping/src/main/java/controller/PageController.java
+++ OnlineShopping/src/main/java/controller/PageController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,30 +11,6 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Spring web MVC");
-
-		return mv;
-	}
-
-//	@RequestMapping(value = "/test")
-//	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) {
-//			
-//		if(greeting==null) {
-//			greeting = "Hello there from if statement";
-//		}
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//
-//		return mv;
-//	}
-	
-	@RequestMapping(value = "/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting") String greeting) {
-			
-		if(greeting==null) {
-			greeting = "Hello there from if statement";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
 
 		return mv;
 	}
